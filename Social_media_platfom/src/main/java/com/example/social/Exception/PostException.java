d499bb65e141fdadcd3e8a9d4401e3c3f1cb0a36
Arijit Bhatta <arijitbhatta123@gmail.com>
2023-04-09 19:35:45 +0530

    Adding Exceptions

---
+++ Social_media_platfom/src/main/java/com/example/social/Exception/PostException.java
@@ -0,0 +1,14 @@
+package com.example.social.Exception;
+
+
+public class PostException extends Exception{
+
+	public PostException() {
+		
+	}
+	
+	public PostException(String s) {
+		super(s);
+	}
+	
+}
---
+++ Social_media_platfom/src/main/java/com/example/social/Exception/UserException.java
@@ -0,0 +1,14 @@
+package com.example.social.Exception;
+
+
+public class UserException extends Exception{
+
+	public UserException() {
+		
+	}
+	
+	public UserException(String s) {
+		super(s);
+	}
+	
+}
